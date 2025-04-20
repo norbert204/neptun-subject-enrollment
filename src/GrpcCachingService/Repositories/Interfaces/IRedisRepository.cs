b2a62428f32552034344ec9a4f9695e6131c0699
bcsuka <csukabalazs1219@gmail.com>
2025-04-20 22:44:36 +0200

    Alap gRPC projekt létrehozása Redis kapcsolattal
    - redis proto file
    - Google proto fileok
    - Redis repository
    - Redis service

---
+++ src/GrpcCachingService/Repositories/Interfaces/IRedisRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace GrpcCachingService.Repositories.Interfaces;
+
+public interface IRedisRepository
+{
+    Task<string> GetRecordAsync(string key);
+    Task<bool> SetRecordAsync(string key, string data, TimeSpan? expiry = null);
+    Task<bool> DeleteRecordAsync(string key);
+}
