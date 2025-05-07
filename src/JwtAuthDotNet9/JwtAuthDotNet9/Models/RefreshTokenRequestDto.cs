ca073825f567c033547e72c29ff07b68da802a86
DEarniroth <arniroth@mailbox.unideb.hu>
2025-05-07 11:05:24 +0200

    Authentication module
    Makes use of gRPC functions to generate and refresh JWT tokens for authentication purposes. Quite a universal solution as it also implements registration and login using local DB.

---
+++ src/JwtAuthDotNet9/JwtAuthDotNet9/Models/RefreshTokenRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtAuthDotNet9.Models
+{
+    public class RefreshTokenRequestDto
+    {
+        public Guid UserID { get; set; }
+        public required string RefreshToken { get; set; }
+    }
+}
