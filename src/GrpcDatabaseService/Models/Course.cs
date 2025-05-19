1258c3873bbcd655ffebebc964a46143dcca9f61
ilosvayaron <ilosvayaron@gmail.com>
2025-05-19 17:11:17 +0200

    Modellek és végpontok módosítása

--- src/GrpcDatabaseService/Models/Course.cs
+++ src/GrpcDatabaseService/Models/Course.cs
@@ -1,22 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace GrpcDatabaseService.Models
+﻿namespace GrpcDatabaseService.Models
 {
-    public enum CourseType
-    {
-        Lecture,
-        Lab
-    }
-
+    /// <summary>
+    /// Represents a course offering for a subject
+    /// </summary>
     public class Course
     {
-        public string ID { get; set; }
+        /// <summary>
+        /// Unique identifier for the course (also serves as owner reference)
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The room where the course is held
+        /// </summary>
         public string Room { get; set; }
-        public string StartTime { get; set; } 
+
+        /// <summary>
+        /// Weekly start time of the course
+        /// </summary>
+        public string StartTime { get; set; }
+
+        /// <summary>
+        /// Weekly end time of the course
+        /// </summary>
         public string EndTime { get; set; }
+
+        /// <summary>
+        /// Maximum number of students that can enroll
+        /// </summary>
         public int Capacity { get; set; }
+
+        /// <summary>
+        /// List of student IDs who are enrolled in the course
+        /// </summary>
         public List<string> EnrolledStudents { get; set; } = new List<string>();
-        public CourseType CourseType { get; set; }
+
+        /// <summary>
+        /// Type of course (lecture, lab, etc.)
+        /// </summary>
+        public string CourseType { get; set; }
     }
 }
--- src/GrpcDatabaseService/Models/Subject.cs
+++ src/GrpcDatabaseService/Models/Subject.cs
@@ -1,13 +1,33 @@
-﻿using System.Collections.Generic;
-
-namespace GrpcDatabaseService.Models
+﻿namespace GrpcDatabaseService.Models
 {
+    /// <summary>
+    /// Represents an academic subject in the system
+    /// </summary>
     public class Subject
     {
-        public string ID { get; set; }
+        /// <summary>
+        /// Unique identifier for the subject
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The owner or instructor of the subject
+        /// </summary>
         public string Owner { get; set; }
+
+        /// <summary>
+        /// The name of the subject
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// List of prerequisite subject IDs required before taking this subject
+        /// </summary>
         public List<string> Prerequisites { get; set; } = new List<string>();
+
+        /// <summary>
+        /// List of course IDs associated with this subject
+        /// </summary>
         public List<string> Courses { get; set; } = new List<string>();
     }
 }
