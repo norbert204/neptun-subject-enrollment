436c7a3e713d862bec03a984538e25b297d34b39
peterpetrov <peterpetrov2003@mailbox.unideb.hu>
2025-12-06 15:07:14 +0100

    add ListSubjectComponent

--- frontend/components/ListSubjectComponent.jsx
+++ frontend/components/ListSubjectComponent.jsx
@@ -1,39 +1,37 @@
-import React from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const ListSubjectComponent = () => {
-
     const dummyData = [
-        {
-            "id": 1,
-            "space": 100,
-            "required": "Java",
-            "name": "Java-2"
-        },
+        { id: 1, space: 100, required: "Java", name: "Java-2" },
+        { id: 2, space: 100, required: "Java-2", name: "Java-3" }
+    ];
+    return (
+        <div className='container'>
+            <h2>Tantárgyak listája</h2>
+            <table className='table table-striped table-bordered'>
+                <thead>
+                    <tr>
+                        <th>Tantárgy azonosító</th>
+                        <th>Tantárgy neve</th>
+                        <th>Férőhelyek</th>
+                        <th>Előfeltétel</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {dummyData.map(subject => (
+                        <tr key={subject.id}>
+                            <td>{subject.id}</td>
+                            <td>{subject.name}</td>
+                            <td>{subject.space}</td>
+                            <td>{subject.required}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
 
-        {
-            "id": 2,
-            "space": 100,
-            "required": "Java-2",
-            "name": "Java-3"
-        }
-    ]
-
-  return (
-    <div>
-
-        <h2>Tantárgyak listája</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>Tantárgy azonosító</th>
-                    <th>Tantárgy neve</th>
-                    <th>Férőhelyek</th>
-                    <th>Előfeltétel</th>
-                </tr>
-            </thead>
-        </table>
-    </div>
-  )
-}
-
-export default ListSubjectComponent
+export default ListSubjectComponent;
