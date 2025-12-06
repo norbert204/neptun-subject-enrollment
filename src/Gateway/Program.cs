2f30734c06505dc708cea5b1c6f8e686ed0e4853
Norbert Horvath <norbert.horvath@ni.com>
2025-12-06 15:30:47 +0100

    Added swagger

--- src/Gateway/Program.cs
+++ src/Gateway/Program.cs
@@ -4,9 +4,10 @@ using SubjectService;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
 
 var serviceLocationOptions = builder.Configuration.GetSection("ServiceLocation").Get<ServiceLocationOptions>();
 
@@ -37,7 +38,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
