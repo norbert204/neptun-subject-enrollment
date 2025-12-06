5ce1c3571d29c7fb11ef483911ef785daa94a999
peterpetrov <peterpetrov2003@mailbox.unideb.hu>
2025-12-06 16:12:45 +0100

    add header and footer

---
+++ frontend/components/HeaderComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className='navbar navbar-dark bg-dark'></nav>
+        </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
