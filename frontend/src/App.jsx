930e5de6c4050d4cd2ef682419956ae85e930af0
Petrov-Todorov Pál <petrovpal2003@gmail.com>
2025-12-09 21:03:06 +0100

    login frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,24 @@ import { useState } from 'react'
 import HeaderComponent from '../components/HeaderComponent'
 import ListStudentComponent from '../components/ListStudentComponent'
 import ListSubjectComponent from '../components/ListSubjectComponent'
+import ListEnrolledSubjectComponent from '../components/ListEnrolledSubjectComponent'
+import LoginComponent from '../components/LoginComponent'
 import './App.css'
 
 function App() {
-  const [activeComponent, setActiveComponent] = useState('students')
+  const [activeComponent, setActiveComponent] = useState('login')
 
   return (
     <>
-      <HeaderComponent activeComponent={activeComponent} setActiveComponent={setActiveComponent} />
+      <HeaderComponent 
+        activeComponent={activeComponent} 
+        setActiveComponent={setActiveComponent} 
+      />
+
       {activeComponent === 'students' && <ListStudentComponent />}
       {activeComponent === 'subjects' && <ListSubjectComponent />}
+      {activeComponent === 'enrolledSubjects' && <ListEnrolledSubjectComponent />}
+      {activeComponent === 'login' && <LoginComponent />}
     </>
   )
 }
