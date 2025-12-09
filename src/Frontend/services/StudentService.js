c21273a6c5221d59569fb3f364813d0a3394e794
Norbert Horvath <norbert.horvath@ni.com>
2025-12-09 20:58:36 +0100

    Used env var in React services

--- src/Frontend/services/StudentService.js
+++ src/Frontend/services/StudentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "https://localhost:5000/user";
+const API_URL = `${process.env.GATEWAY_URI}/user`;
 
 export const getAllStudents = () => {
     return axios.get(API_URL);
--- src/Frontend/services/SubjectService.js
+++ src/Frontend/services/SubjectService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const REST_API_BASE_URL = "http://localhost:5000/Subject";
+const REST_API_BASE_URL = `${process.env.GATEWAY_URI}/Subject`;
 
 export const getEligibleCoursesForStudent = (studentId) =>
   axios.get(`${REST_API_BASE_URL}/student/${studentId}/eligible-courses`);
